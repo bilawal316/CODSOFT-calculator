1ef1b79d878e584021d43afdcf6c7a4094fd1343
M-Bilawal Zaman <mbilawalzaman@gmail.com>
2023-09-20 17:44:22 +0500

    modified: app/Components/Calculator/calculator.js, app/globals.css, app/page.tsx

--- app/Components/Calculator/calculator.js
+++ app/Components/Calculator/calculator.js
@@ -22,9 +22,9 @@ function Calculator() {
 
   return (
     <div className="border bg-black p-4 rounded-lg">
-    <div className="border mb-2 text-center text-black" style={{ backgroundColor: "lightblue", minHeight: "2rem" }}>
-    {display}
-  </div>
+      <div className="border mb-2 text-center text-black" style={{ backgroundColor: "lightblue", minHeight: "2rem" }}>
+        {display}
+      </div>
       <div className=" grid grid-cols-4 gap-2">
         <button className="calculatorButton" onClick={() => handleClick("1")}>
           1
@@ -68,10 +68,10 @@ function Calculator() {
         <button className="calculatorButton" onClick={() => handleClick(".")}>
           .
         </button>
-        <button className="calculatorButton" onClick={calculateResult}>
+        <button className="calculatorButton bg-green-500" onClick={calculateResult}>
           =
         </button>
-        <button className="calculatorButton bg-red-500" onClick={clearDisplay}>
+        <button className="calculatorButton bg-red-500 " onClick={clearDisplay}>
           C
         </button>
       </div>
